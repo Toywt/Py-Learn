423ba26d0a7e3a2d4ccb1743533461d7731b3dcc
Toywt <wangtao813@foxmail.com>
2020-03-15 22:14:21 +0800

    完成2-3的练习

---
+++ full_name.py
@@ -0,0 +1,4 @@
+first_name = 'wang'
+last_name = 'tao'
+full_name = first_name + ' ' + last_name
+print(full_name)
---
+++ name.py
@@ -0,0 +1,4 @@
+name = 'wang tao'
+print(name.title())
+print(name.upper())
+print(name.lower())
