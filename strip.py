96f599fee2fbb5972608d67df3fa1d507957a95d
Toywt <wangtao813@foxmail.com>
2020-03-17 22:15:50 +0800

    完成2-3.3练习

---
+++ strip.py
@@ -0,0 +1,9 @@
+languges = ' python '
+print(languges)
+r_languges = languges.rstrip()
+l_languges = languges.lstrip()
+languges = languges.strip()
+
+print('删除右边边的空白' + r_languges)
+print('删除左边的空白' + l_languges)
+print('删除两端的空白' + languges)
